b155f2e0e9e8dc63ba80fd77d0876065314e5dfc
kumarnutan204 <kumarnutan204@gmail.com>
2023-01-18 23:12:26 +0530

    added vector of pairs

---
+++ VectorofPairs.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printVec(vector<pair<int,int>>&v){
+    cout<<"Size:" <<v.size()<<endl;
+    for(int i=0;i<v.size();i++)
+    {   //v.size( ) has a complexity of 0(1);
+        cout<< v[i].first<< " " << v[i].second<< endl;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    vector<pair<int,int>> v = {{1,2},{2,3},{3,4}};
+    printVec(v);
+    return 0;
+}
