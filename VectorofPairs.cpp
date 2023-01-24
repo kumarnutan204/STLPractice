2375f689e93817f3d6cf636a683cebb659fc40d9
kumarnutan204 <kumarnutan204@gmail.com>
2023-01-24 22:22:32 +0530

    Vector of Pairs

--- VectorofPairs.cpp
+++ VectorofPairs.cpp
@@ -14,6 +14,19 @@ int main()
 {
     vector<pair<int,int>> v = {{1,2},{2,3},{3,4}};
     printVec(v);
+    cout<<"enter the number of pairs in the vector ";
+    int n;
+    cin>>n;
+    cout<<"Enter the pairs";
+    vector<pair<int,int>>v2;
+    for(int i=0;i<n;i++)
+    {   
+        int x,y;
+        cin>>x>>y;
+        v2.push_back({x,y});
+     
+    }
+    printVec(v2);
     
     return 0;
 }
